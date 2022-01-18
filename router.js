8d4d5f2926c3c0402926a2cc64c1f04be5e7b96b
Thái Chí Bảo <comaybayy@gmail.com>
2022-01-18 16:24:54 +0700

    @nuxtjs/router installation

---
+++ router.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import HomePage from '~/pages/index'
+import ListingPage from '~/pages/listing/index'
+
+Vue.use(Router)
+
+export function createRouter () {
+  return new Router({
+    mode: 'history',
+    routes: [
+      {
+        path: '/',
+        component: HomePage
+      },
+      {
+        path: '/danh-sach-can-ho/:slug',
+        component: ListingPage
+      }
+    ]
+  })
+}

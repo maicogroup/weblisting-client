9fd2fe7984ced31c0ff314c171cb00f529198e34
QuocPhongMaiCO <93976658+QuocPhongMaiCO@users.noreply.github.com>
2022-01-19 16:03:39 +0700

    Phong-create-listing

---
+++ pages/list-posts.vue
@@ -0,0 +1,163 @@
+<template>
+    <div class="max-w-full mx-auto">
+        <div class="p-4 bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <div class="flex justify-between items-center">
+              <p class="text-sm truncate mar-address">
+                    Hiện có: 19,384 bất động sản
+              </p>
+              <div>
+                  <p class="text-sm truncate mar-address">
+                      Sắp xếp theo:
+                  </p>
+                  <a class="text-sm truncate mar-address" href="#">Mặc định </a>
+                  <i class='bx bxs-chevron-down'></i>
+              </div>
+            </div>
+   <div class="flow-root">
+        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+            <li class="py-3 border-none">
+                <div class="flex item-flex-start space-x-4 rounded-md border">
+                        <img class="w-60 h-40 rounded-md border" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Bonnie image">
+                    <div class="flex-1 min-w-0">
+                        <p class="font-bold text-lg leading-6 mar-title h-12 ov-flow-hidden">
+                            CĂN HỘ HUHIHAHA TẦNG 6 BAN CÔNG RỘNG RÃI THOÁNG MÁT VÃI ĐẠN AHIHI FIRST THING FIRST PRERESENT TO CD TOWN
+                        </p>
+                        <p class="font-price mar-price">
+                            6,3 Tỷ - 52 m² - 2PN3WC
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400 mar-address">
+                            8 Miles, Novara, CĐ City
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            Hôm nay
+                        </p>
+                    </div>
+                </div>
+            </li>
+            <li class="py-3 border-none">
+                <div class="flex item-flex-start space-x-4 rounded-md border">
+                        <img class="w-60 h-40 rounded-md" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Bonnie image">
+                    <div class="flex-1 min-w-0">
+                        <p class="font-bold text-lg leading-6 mar-title h-12 ov-flow-hidden">
+                            CĂN HỘ HUHIHAHA TẦNG 6 BAN CÔNG RỘNG RÃI THOÁNG MÁT VÃI ĐẠN AHIHI FIRST THING FIRST PRERESENT TO CD TOWN
+                        </p>
+                        <p class="font-price mar-price">
+                            6,3 Tỷ - 52 m² - 2PN3WC
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400 mar-address">
+                            8 Miles, Novara, CĐ City
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            Hôm nay
+                        </p>
+                    </div>
+                </div>
+            </li>
+            <li class="py-3 border-none">
+                <div class="flex item-flex-start space-x-4 rounded-md border">
+                        <img class="w-60 h-40 rounded-md" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Bonnie image">
+                    <div class="flex-1 min-w-0">
+                        <p class="font-bold text-lg leading-6 mar-title h-12 ov-flow-hidden">
+                            CĂN HỘ HUHIHAHA TẦNG 6 BAN CÔNG RỘNG RÃI THOÁNG MÁT VÃI ĐẠN AHIHI FIRST THING FIRST PRERESENT TO CD TOWN
+                        </p>
+                        <p class="font-price mar-price">
+                            6,3 Tỷ - 52 m² - 2PN3WC
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400 mar-address">
+                            8 Miles, Novara, CĐ City
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            Hôm nay
+                        </p>
+                    </div>
+                </div>
+            </li>
+            <li class="py-3 border-none">
+                <div class="flex item-flex-start space-x-4 rounded-md border">
+                        <img class="w-60 h-40 rounded-md" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Bonnie image">
+                    <div class="flex-1 min-w-0">
+                        <p class="font-bold text-lg leading-6 mar-title h-12 ov-flow-hidden">
+                            CĂN HỘ HUHIHAHA TẦNG 6 BAN CÔNG RỘNG RÃI THOÁNG MÁT VÃI ĐẠN AHIHI FIRST THING FIRST PRERESENT TO CD TOWN
+                        </p>
+                        <p class="font-price mar-price">
+                            6,3 Tỷ - 52 m² - 2PN3WC
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400 mar-address">
+                            8 Miles, Novara, CĐ City
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            Hôm nay
+                        </p>
+                    </div>
+                </div>
+            </li>
+            <li class="py-3 border-none">
+                <div class="flex item-flex-start space-x-4 rounded-md border">
+                        <img class="w-60 h-40 rounded-md" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="Bonnie image">
+                    <div class="flex-1 min-w-0">
+                        <p class="font-bold text-lg leading-6 mar-title h-12 ov-flow-hidden">
+                            CĂN HỘ HUHIHAHA TẦNG 6 BAN CÔNG RỘNG RÃI THOÁNG MÁT VÃI ĐẠN AHIHI FIRST THING FIRST PRERESENT TO CD TOWN
+                        </p>
+                        <p class="font-price mar-price">
+                            6,3 Tỷ - 52 m² - 2PN3WC
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400 mar-address">
+                            8 Miles, Novara, CĐ City
+                        </p>
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            Hôm nay
+                        </p>
+                    </div>
+                </div>
+            </li>
+        </ul>
+   </div>
+</div>
+    <p class="mt-5">This card component is part of a larger, open-source library of Tailwind CSS components. Learn more by going to the official
+        <a class="text-blue-600 hover:underline" href="https://flowbite.com/docs/getting-started/introduction/" target="_blank">Flowbite Documentation</a>.
+    </p>
+</div>
+</template>
+
+<style scoped>
+.w-h-100{
+    width: 100px;
+    height: 100px;
+}
+
+.item-flex-start{
+    align-items: flex-start;
+}
+
+.font-price{
+    line-height: 16.43px;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 700;
+}
+
+.mar-title{
+    margin: 10px 76px 14px 0;
+}
+
+.mar-price{
+    margin-bottom: 18px;
+}
+
+.mar-address{
+    margin-bottom: 15px;
+}
+
+.ov-flow-hidden{
+    overflow: hidden;
+}
+
+.border-none{
+    border: none;
+}
+</style>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({ setup () {} })
+</script>

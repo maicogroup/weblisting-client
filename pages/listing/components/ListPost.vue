6799c40e401403d62a1a69ddbc8d2b726f2056f4
Thái Chí Bảo <comaybayy@gmail.com>
2022-01-21 15:44:30 +0700

    add ContactInfor

--- pages/listing/components/ListPost.vue
+++ pages/listing/components/ListPost.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="max-w-full">
-            <div class="flex justify-between items-center mt-2 mb-2">
+    <div class="max-w-full" id="post-subinfor">
+            <div class="flex justify-between items-center mt-2 mb-2" style="height: 2.5rem;">
               <p class="md:text-sm text-xs truncate mar-address">
-                    Hiện có: 19,384 bất động sản
+                    Hiện có: {{totalItem}} bất động sản
               </p>
               <div>
                   <p class="inline text-sm truncate mar-address">
                       Sắp xếp theo:
                   </p>
-                  <Dropdown class="text-sm md:mr-9"> <DropdownItem>
+                  <Dropdown class="text-sm md:mr-9"> 
+                      <DropdownItem>
                           Giá cao nhất
                       </DropdownItem>
                       <DropdownItem>
@@ -22,7 +23,7 @@
             </div>
    <div class="flow-root">
         <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
-            <li class="pb-6 md:pt-0 pt-6  md:border-none border-b border-black" v-for="post in posts" :key="post">
+            <li class="pb-6 md:pt-0 pt-6  md:border-none border-b border-black" v-for="post in posts" :key="post.id">
                 <div class="">
                     <p class="md:hidden truncate ... font-bold text-lg leading-6 ov-flow-hidden mb-3">
                             {{post.title}}
@@ -48,6 +49,7 @@
             </li>
         </ul>
    </div>
+    <pagination :total="totalItem" :perPage="10" :currentPage="pageIndex" @pagechanged="pageNavigationTo"></pagination>
     </div>
 </template>
 
@@ -89,40 +91,114 @@
 }
 </style>
 <script>
-import Dropdown from '~/pages/components/Dropdown/Dropdown.vue';
-import DropdownItem from '~/pages/components/Dropdown/DropdownItem.vue';
-export default { components: { Dropdown, DropdownItem }, name: 'ListPost', data(){
-    return{
-        posts : [{
-            srcimage: 'https://flowbite.com/docs/images/people/profile-picture-3.jpg',
-            title: 'CĂN HỘ HUHIHAHA TẦNG 6 BAN CÔNG RỘNG RÃI THOÁNG MÁT VÃI ĐẠN AHIHI FIRST THING FIRST PRERESENT TO CD TOWN',
-            price: '6,3 Tỷ - 52 m² - 2PN3WC',
-            address: '8 Miles, Novara, CĐ City',
-            date: 'Hôm nay'
-        },
-        {
-            srcimage: 'https://flowbite.com/docs/images/people/profile-picture-3.jpg',
-            title: 'CĂN HỘ HUHIHAHA TẦNG 6 BAN CÔNG RỘNG RÃI THOÁNG MÁT VÃI ĐẠN AHIHI FIRST THING FIRST PRERESENT TO CD TOWN',
-            price: '6,3 Tỷ - 52 m² - 2PN3WC',
-            address: '8 Miles, Novara, CĐ City',
-            date: 'Hôm nay'
-        },
-        {
-            srcimage: 'https://flowbite.com/docs/images/people/profile-picture-3.jpg',
-            title: 'CĂN HỘ HUHIHAHA TẦNG 6 BAN CÔNG RỘNG RÃI THOÁNG MÁT VÃI ĐẠN AHIHI FIRST THING FIRST PRERESENT TO CD TOWN',
-            price: '6,3 Tỷ - 52 m² - 2PN3WC',
-            address: '8 Miles, Novara, CĐ City',
-            date: 'Hôm nay'
-        },
-        {
-            srcimage: 'https://flowbite.com/docs/images/people/profile-picture-3.jpg',
-            title: 'CĂN HỘ HUHIHAHA TẦNG 6 BAN CÔNG RỘNG RÃI THOÁNG MÁT VÃI ĐẠN AHIHI FIRST THING FIRST PRERESENT TO CD TOWN',
-            price: '6,3 Tỷ - 52 m² - 2PN3WC',
-            address: '8 Miles, Novara, CĐ City',
-            date: 'Hôm nay'
+import { computed } from '@vue/composition-api';
+import {gql} from 'graphql-tag';
+// import pagination from '~/pages/components/pagination.vue';
+// import Dropdown from '~/pages/components/Dropdown/Dropdown.vue';
+// import DropdownItem from '~/pages/components/Dropdown/DropdownItem.vue';
+
+const getPostsQuery = gql`
+          query GetPostWithPagination($condition: PostCollectionFilterInput, $skipItems: Int, $take: Int, $order : [PostCollectionSortInput!]) {
+            postsWithPagination(take: $take, skip: $skipItems, where: $condition, order : $order) {
+              items{
+                id
+                pageInfor{
+                  title
+                  slug
+                  metaDescription
+                }
+                gallery
+                price
+                description
+                demand
+                status,
+                acreage,
+                roomStructure,
+                apartmentState,
+                tags
+                project{
+                    address{
+                        street
+                        city
+                        district
+                        googleMapLocation
+                    }
+                }
+                lastUpdatedAt
+              }
+              totalCount
+            }
+          }
+        `;
+
+export default { 
+    props:[
+        "projectId"
+    ],
+    name: 'ListPost', 
+    data(){
+        return{
+            pageIndex: 1,
+            pageOfItems: []
         }
-        ]
+    },
+    apollo:{
+        postsData:{
+            query(){
+                return getPostsQuery
+            },
+            update: data => data.postsWithPagination,
+            skip(){
+                return this.projectId == null;
+            },
+            variables(){
+                return{
+                    condition: {
+                        projectId:{
+                            eq : this.projectId
+                        }
+                    },
+                    take: 10,
+                }
+            }
+        },
+    },
+    methods:{
+        pageNavigationTo: function(index){
+            
+            this.$apollo.queries.postsData.refetch ({
+                condition: {
+                    projectId:{
+                        eq : this.projectId
+                    }
+                },
+                take: 10,
+                skipItems: 10 * (index - 1)
+            });
+            this.pageIndex = index;
+            document.getElementById('post-subinfor').scrollIntoView();
+        }
+    },
+    computed:{
+        posts(){
+            if(this.postsData == null) return []
+            else
+                return this.postsData.items.map(function(item){
+                    return {
+                        srcimage: 'https://maico-hub-record.ss-hn-1.bizflycloud.vn/' + (item.gallery.find(c => !c.includes('.mp4'))|| 'apartment-resource/00800a5f-eb0c-4c6f-93ad-1c28e03b70dc/17-01-2022_0953/image/z3116547105303_32a851d4f5d44bca12e64ac1a09e6a6d.jpg'),
+                        title: item.pageInfor.title,
+                        price: item.price + ' ' + item.acreage + ' ' + item.roomStructure,
+                        address: item.project.address.street + ' ' + item.project.address.district + ' ' + item.project.address.city,
+                        date: 'Cập nhật lần cuối: ' + item.lastUpdatedAt,
+                        id: item.id
+                    }
+                });
+        },
+        totalItem(){
+            if(this.postsData == null) return 0
+            else
+                return this.postsData.totalCount;
+        }
     }
-} 
 };
 </script>

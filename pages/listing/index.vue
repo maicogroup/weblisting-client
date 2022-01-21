5353b4b42453edfd1c86a51762a60918600652d1
Thái Chí Bảo <comaybayy@gmail.com>
2022-01-21 15:44:30 +0700

    add ContactInfor

--- pages/listing/index.vue
+++ pages/listing/index.vue
@@ -3,8 +3,8 @@
     <ProjectHeader v-if="project != null" :project="project" />
     <Divider class="mt-7 mb-1.5" />
     <div class="flex justify-between w-full">
-    <ListPost class="left-0" />
-    <ContactInfor class="lg:ml-9 hidden lg:flex lg:flex-col mt-10" />
+      <ListPost class="left-0" />
+      <ContactInfor class="lg:ml-9 hidden lg:flex lg:flex-col mt-10" />
     </div>
   </div>
 </template>

3192e98cb258d5dc7e2d869afe7ec43244c76933
Thái Chí Bảo <comaybayy@gmail.com>
2022-02-09 15:14:54 +0700

    fix ui product review page

--- pages/product-preview/specific-project/index.vue
+++ pages/product-preview/specific-project/index.vue
@@ -3,17 +3,17 @@
     <div v-if="project != null" class="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div>
         <img class="w-full h-60 object-cover lg:h-80" :src="project.images[0]">
-        <h4 class="uppercase mt-4 text-xl font-bold px-4 md:px-0 lg:mt-2">
+        <h4 class="uppercase mt-4 text-xl font-bold lg:mt-2">
           Căn hộ chung cư {{ project.projectName }}
         </h4>
-        <p class="text-gray-500 px-4 md:px-0">
+        <p class="text-gray-500">
           {{ projectAddress }}
         </p>
-        <nuxt-link to="/" class="hover:text-green-500 px-4 md:px-0">
+        <nuxt-link to="/" class="hover:text-green-500">
           Xem chi tiết về chung cư {{ project.projectName }} >>
         </nuxt-link>
       </div>
-      <iframe :src="project.address.googleMapLocation" width="100%" class="h-60 mt-4 lg:mt-0 lg:h-full" loading="lazy" />
+      <iframe :src="project.address.googleMapLocation" class="w-full h-60 md:h-full" loading="lazy" />
     </div>
 
     <divider class="mt-6 mb-6" />

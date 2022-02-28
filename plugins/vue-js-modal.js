6a10a2e1c14637ceb6d2aea1ee396d773b02d9d1
nguyentuankhiem <85607235+nguyentuankhiem@users.noreply.github.com>
2022-02-28 19:46:17 +0700

    Feature/edit data in post edit (#8)
    * fetch data with slug condition
    
    * check slug if already exists
    
    * refresh page if user change slug
    
    * add drag and drop component
    
    * fin drag n drop image
    
    Co-authored-by: minhnhatdo <minhnhat.maicogroup@gmail.com>

--- plugins/vue-js-modal.js
+++ plugins/vue-js-modal.js
@@ -1,6 +1,7 @@
-import Vue from 'vue';
-import VModal from 'vue-js-modal/dist/ssr.nocss';
 
-import 'vue-js-modal/dist/styles.css';
+import Vue from 'vue'
+import VModal from 'vue-js-modal/dist/ssr.nocss'
+
+import 'vue-js-modal/dist/styles.css'
 
 Vue.use(VModal)

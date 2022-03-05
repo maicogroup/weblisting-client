6fb1a2408ce28150066db9c5199b76c38fc7c35e
Thái Chí Bảo <comaybayy@gmail.com>
2022-03-05 14:05:10 +0700

    filter mobile: truncate if needed

--- pages/listing/components/responsive-filtering/direction-filter-options.vue
+++ pages/listing/components/responsive-filtering/direction-filter-options.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <div class="w-full flex justify-between border rounded h-11 items-center px-3 mb-4" @click="open = !open">
-      <p> {{ displaySelected }} </p>
+    <div class="w-full flex justify-between border rounded h-11 items-center px-3 mb-4 " @click="open = !open">
+      <p class="truncate ...">
+        {{ displaySelected }}
+      </p>
       <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M1 13L7 7L1 1" stroke="#999999" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
       </svg>

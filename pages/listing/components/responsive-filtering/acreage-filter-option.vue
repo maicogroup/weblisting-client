a4ee0ff5a3be8194070eaee7ee53fabf283ce010
Thái Chí Bảo <comaybayy@gmail.com>
2022-03-05 13:39:21 +0700

    filter mobile: remove custom range

--- pages/listing/components/responsive-filtering/acreage-filter-option.vue
+++ pages/listing/components/responsive-filtering/acreage-filter-option.vue
@@ -14,36 +14,27 @@
       <div
         class="flex justify-between border-b rounded"
       >
-        <div @click="open = false" class="absolute top-1.5 left-2" >
-          <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="#32c82b" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H6M12 5l-7 7 7 7"/></svg>
+        <div class="absolute top-1.5 left-2" @click="open = false">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="26"
+            height="26"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#32c82b"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ><path d="M19 12H6M12 5l-7 7 7 7" /></svg>
         </div>
         <p class="font-semibold px-6 py-2  mr-auto ml-auto ">
           Chọn diện tích
         </p>
-       
       </div>
-
-      <div class="flex justify-center items-center px-6 mt-3">
-        <label class="flex items-center">
-          <input :value="customRange.from" :class="`w-14 px-2 py-1 text-center text-sm border rounded-md outline-none ${borderRedIfInvalid}`" @input="handleCustomRangeFromChanged">
-          <span class="ml-1">{{ priceUnit }}</span>
-        </label>
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mx-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M20 12H4" />
-        </svg>
-        <label class="flex items-center">
-          <input :value="customRange.to" :class="`w-14 px-2 py-1 text-center text-sm border rounded-md outline-none ${borderRedIfInvalid}`" @input="handleCustomRangeToChanged">
-          <span class="ml-1">{{ priceUnit }}</span>
-        </label>
-      </div>
-
-      <p :class="`mt-1 px-4 text-xs text-red-700 ${showIfInvalid}`">
-        *Khoảng diện tích không hợp lệ
-      </p>
 
       <div class="flex justify-between h-11 border-b items-center mx-3">
         <p>Tất cả diện tích</p>
-        <input type="radio"  checked="checked" @click="handleSelectAcreageRange(range)">
+        <input type="radio" checked="checked" @click="handleSelectAcreageRange(range)">
       </div>
       <div
         v-for="range in acreageRanges"
@@ -89,75 +80,24 @@ export default {
       entered: false,
       displaySelected: 'Diện tích',
       acreageRanges: [{ from: 0, to: 30 }, { from: 30, to: 50 }, { from: 50, to: 80 }, { from: 80, to: 100 }, { from: 100, to: 300 }, { from: 300, to: 500 }, { from: 500, to: null }],
-      customRange: { from: 0, to: 0 },
-      prevCustomRange: { from: 0, to: 0 },
       MAX_VALUE: 999999
     };
   },
 
-  computed: {
-    customRangeIsInvalid () {
-      return (this.customRange.from > this.customRange.to);
-    },
-
-    showIfInvalid () {
-      return this.customRangeIsInvalid ? '' : 'hidden';
-    },
-
-    borderRedIfInvalid () {
-      return this.customRangeIsInvalid ? 'border-red-700' : '';
-    },
-
-    disabilityClasses () {
-      return this.customRangeIsInvalid ? 'text-gray-400 cursor-not-allowed' : 'hover:text-white hover:bg-gray-700';
+  watch: {
+    selectedOption: {
+      handler (option) {
+        if (option) {
+          this.displaySelected = 'Diện tích: ' + this.formatRange(option);
+        } else {
+          this.displaySelected = 'Diện tích';
+        }
+      },
+      immediate: true
     }
   },
 
   methods: {
-    resetCustomRange () {
-      this.customRange = { from: 0, to: 0 };
-    },
-
-    applyCustomRange () {
-      this.displaySelected = this.formatRange(this.customRange);
-      this.prevCustomRange = { ...this.customRange };
-
-      if (this.customRange.from === 0 && this.customRange.to === 0) {
-        // chọn tất cả
-        this.$emit('optionchanged', null);
-      } else {
-        this.$emit('optionchanged', { ...this.customRange });
-      }
-    },
-
-    handleCustomRangeFromChanged (e) {
-      if (e.target.value.trim() === '') {
-        this.customRange = { ...this.customRange, from: 0 };
-        return;
-      }
-
-      const value = parseInt(e.target.value);
-      if (!isNaN(value)) {
-        this.customRange = { ...this.customRange, from: (value > this.MAX_VALUE) ? this.MAX_VALUE : value };
-      } else {
-        this.customRange = { ...this.customRange };
-      }
-    },
-
-    handleCustomRangeToChanged (e) {
-      if (e.target.value.trim() === '') {
-        this.customRange = { ...this.customRange, to: 0 };
-        return;
-      }
-
-      const value = parseInt(e.target.value);
-      if (!isNaN(value)) {
-        this.customRange = { ...this.customRange, to: (value > this.MAX_VALUE) ? this.MAX_VALUE : value };
-      } else {
-        this.customRange = { ...this.customRange };
-      }
-    },
-
     formatRange (range) {
       if (!range.from || range.from === 0) {
         return range.to === 0 ? 'Tất cả' : `Dưới ${range.to} m²`;
@@ -169,23 +109,12 @@ export default {
     },
 
     handleSelectAcreageRange (range) {
-      this.displaySelected = this.formatRange(range);
       this.open = false;
-      if (range.to !== null) {
-        this.prevCustomRange = { ...range };
-        this.customRange = { ...range };
-      } else {
-        this.prevCustomRange = { from: 0, to: 0 };
-        this.customRange = { from: 0, to: 0 };
-      }
       this.$emit('optionchanged', range);
     },
 
     handleSelectAllSize () {
-      this.displaySelected = 'Tất cả';
       this.open = false;
-      this.prevCustomRange = { from: 0, to: 0 };
-      this.customRange = { from: 0, to: 0 };
       this.$emit('optionchanged', null);
     }
   }
--- pages/listing/components/responsive-filtering/price-filter-option.vue
+++ pages/listing/components/responsive-filtering/price-filter-option.vue
@@ -14,32 +14,23 @@
       <div
         class="flex justify-between border-b rounded"
       >
-        <div @click="open = false" class="absolute top-1.5 left-2" >
-          <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="#32c82b" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><path d="M19 12H6M12 5l-7 7 7 7"/></svg>
+        <div class="absolute top-1.5 left-2" @click="open = false">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="26"
+            height="26"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#32c82b"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ><path d="M19 12H6M12 5l-7 7 7 7" /></svg>
         </div>
         <p class="font-semibold px-6 py-2  mr-auto ml-auto ">
           Chọn giá
         </p>
-        
       </div>
-
-      <div class="flex justify-center items-center px-6 mt-3">
-        <label class="flex items-center">
-          <input :value="customRange.from" :class="`w-14 px-2 py-1 text-center text-sm border rounded-md outline-none ${borderRedIfInvalid}`" @input="handleCustomRangeFromChanged">
-          <span class="ml-1">{{ priceUnit }}</span>
-        </label>
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mx-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M20 12H4" />
-        </svg>
-        <label class="flex items-center">
-          <input :value="customRange.to" :class="`w-14 px-2 py-1 text-center text-sm border rounded-md outline-none ${borderRedIfInvalid}`" @input="handleCustomRangeToChanged">
-          <span class="ml-1">{{ priceUnit }}</span>
-        </label>
-      </div>
-
-      <p :class="`mt-1 px-4 text-xs text-red-700 ${showIfInvalid}`">
-        *Khoảng giá không hợp lệ
-      </p>
 
       <div class="flex justify-between h-11 border-b items-center mx-3">
         <p>Tất cả mức giá</p>
@@ -89,8 +80,6 @@ export default {
       open: false,
       entered: false,
       displaySelected: 'Giá',
-      customRange: { from: 0, to: 0 },
-      prevCustomRange: { from: 0, to: 0 },
       MAX_VALUE: 999999
     };
   },
@@ -106,21 +95,6 @@ export default {
       } else {
         return [{ from: 0, to: 3 }, { from: 3, to: 5 }, { from: 5, to: 7 }, { from: 7, to: 10 }, { from: 10, to: 15 }, { from: 15, to: 20 }, { from: 25, to: 30 }, { from: 30, to: null }];
       }
-    },
-
-    customRangeIsInvalid () {
-      return (this.customRange.from > this.customRange.to);
-    },
-    showIfInvalid () {
-      return this.customRangeIsInvalid ? '' : 'hidden';
-    },
-
-    borderRedIfInvalid () {
-      return this.customRangeIsInvalid ? 'border-red-700' : '';
-    },
-
-    disabilityClasses () {
-      return this.customRangeIsInvalid ? 'text-gray-400 cursor-not-allowed' : 'hover:text-white hover:bg-gray-700';
     }
   },
 
@@ -128,11 +102,9 @@ export default {
     selectedOption: {
       handler (option) {
         if (option) {
-          this.displaySelected = this.formatRange(option);
-          this.setCustomRange(option);
+          this.displaySelected = 'Giá: ' + this.formatRange(option);
         } else {
           this.displaySelected = 'Giá';
-          this.setCustomRange({ from: 0, to: 0 });
         }
       },
       immediate: true
@@ -149,45 +121,6 @@ export default {
       this.customRange = { from: 0, to: 0 };
     },
 
-    applyCustomRange () {
-      this.prevCustomRange = { ...this.customRange };
-
-      if (this.customRange.from === 0 && this.customRange.to === 0) {
-        // chọn tất cả
-        this.$emit('optionchanged', null);
-      } else {
-        this.$emit('optionchanged', { ...this.customRange });
-      }
-    },
-
-    handleCustomRangeFromChanged (e) {
-      if (e.target.value.trim() === '') {
-        this.customRange = { ...this.customRange, from: 0 };
-        return;
-      }
-
-      const value = parseInt(e.target.value);
-      if (!isNaN(value)) {
-        this.customRange = { ...this.customRange, from: (value > this.MAX_VALUE) ? this.MAX_VALUE : value };
-      } else {
-        this.customRange = { ...this.customRange };
-      }
-    },
-
-    handleCustomRangeToChanged (e) {
-      if (e.target.value.trim() === '') {
-        this.customRange = { ...this.customRange, to: 0 };
-        return;
-      }
-
-      const value = parseInt(e.target.value);
-      if (!isNaN(value)) {
-        this.customRange = { ...this.customRange, to: (value > this.MAX_VALUE) ? this.MAX_VALUE : value };
-      } else {
-        this.customRange = { ...this.customRange };
-      }
-    },
-
     formatRange (range) {
       const unit = this.demand === 'Bán' ? 'tỷ' : 'triệu';
 
@@ -202,24 +135,11 @@ export default {
 
     handleSelectPriceRange (range) {
       this.open = false;
-      this.setCustomRange(range);
       this.$emit('optionchanged', { ...range });
-    },
-
-    setCustomRange (range) {
-      if (range.to) {
-        this.prevCustomRange = { ...range };
-        this.customRange = { ...range };
-      } else {
-        this.prevCustomRange = { from: 0, to: 0 };
-        this.customRange = { from: 0, to: 0 };
-      }
     },
 
     handleSelectAllPrices () {
       this.open = false;
-      this.prevCustomRange = { from: 0, to: 0 };
-      this.customRange = { from: 0, to: 0 };
       this.$emit('optionchanged', null);
     }
   }

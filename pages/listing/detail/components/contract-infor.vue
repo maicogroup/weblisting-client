dd59fabf2bd532a1408d0841ef9b301143e8468b
Thái Chí Bảo <comaybayy@gmail.com>
2022-03-10 17:26:42 +0700

    contact-infor: remove review and report buttons

--- pages/listing/detail/components/contract-infor.vue
+++ pages/listing/detail/components/contract-infor.vue
@@ -20,16 +20,6 @@
         </button>
       </div>
     </div>
-    <div class="w-full border-b bd-color-858585 mt-6 mb-3" />
-    <button class="bd-color-858585 color-707070 rounded w-64 h-10 border text-sm font-bold mb-3 hover:text-white hover:bg-gray-700" @click="featureNotSupported">
-      Xem review dự án
-    </button>
-    <button class="bd-color-858585 color-707070 rounded w-64 h-10 border text-sm font-bold flex items-center justify-center hover:text-white hover:bg-gray-700" @click="featureNotSupported">
-      <svg xmlns="http://www.w3.org/2000/svg" class="mr-1 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
-      </svg>
-      Báo cáo
-    </button>
   </div>
 </template>
 

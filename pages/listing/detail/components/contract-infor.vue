cb44b9fb949db9c87b1bd9748624bc2a8bc208e4
Thái Chí Bảo <comaybayy@gmail.com>
2022-03-11 10:06:01 +0700

    contact infor desktop redesign

--- pages/listing/detail/components/contract-infor.vue
+++ pages/listing/detail/components/contract-infor.vue
@@ -1,24 +1,23 @@
 <template>
   <div>
-    <div class="bd-color-c4c4c4 grid grid-rows-4 grid-flow-col flex gap-4 border w-64 h-72 rounded-md px-3.5 pb-3 justify-center">
-      <div class="items-center flex flex-col mt-9">
-        <img
-          class="inline-block h-14 w-14 rounded-full ring-2 ring-white mb-6"
-          src="http://crm.maicogroup.net/assets/images/logo.png"
-          alt="maico"
-        >
-        <h3 class="text-sm color-707070 leading-4 font-bold">
-          Maico
-        </h3>
-      </div>
-      <div class="mt-20">
-        <button class="bd-color-858585 color-707070 rounded w-56 h-10 border text-sm font-bold mb-4 hover:text-white hover:bg-gray-700" @click="featureNotSupported">
-          Yêu cầu liên hệ lại
-        </button>
-        <button class="rounded w-56 h-10 border bg-green-600 hover:bg-green-700 text-white text-sm font-bold flex items-center justify-center">
-          <a href="tel:0903324045">0903 324 045</a>
-        </button>
-      </div>
+    <div class="shadow border-gray border w-72 rounded-md px-6 py-4 flex flex-col items-center">
+      <img
+        class="inline-block object-cover h-14 w-14 rounded-full border border-gray"
+        src="~/assets/maico-contact-infor.png"
+        alt="maico"
+      >
+      <p class="mt-1 text-green-600 font-bold leading-4">
+        MAICO
+      </p>
+      <a href="tel:0961677167" class="mt-3 rounded-md w-56 h-10 border bg-green-600 hover:bg-green-700 text-white text-sm font-bold flex items-center justify-center">
+        <svg xmlns="http://www.w3.org/2000/svg" class="mr-1 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 3h5m0 0v5m0-5l-6 6M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
+        </svg>
+        <p>096 167 71 67</p>
+      </a>
+      <button class="mt-2 text-sm text-green-600 rounded-md w-56 h-10 border border-green-600 text-sm font-bold hover:text-green-700 hover:border-green-700 hover:bg-gray-100" @click="featureNotSupported">
+        Yêu cầu tư vấn
+      </button>
     </div>
   </div>
 </template>
@@ -40,17 +39,11 @@ export default {
 </script>
 
 <style scoped>
-.color-707070{
-  color: #707070;
-}
+  .shadow {
+    box-shadow: 0px 2px 20px #ebebeb;
+  }
 
-.bd-color-c4c4c4
-{
-  border-color: #C4C4C4;
-}
-
-.bd-color-858585
-{
-  border-color: #858585;
-}
+  .border-gray {
+    border-color: #E7E5E4;
+  }
 </style>

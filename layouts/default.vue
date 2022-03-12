58ee8ef6b273e56a43f9fb1cf6c91d3b95891ccd
quanglam2003 <quanglam.maicogroup@gmail.com>
2022-03-12 21:17:33 +0700

    fix contact-infor border and spacing

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,7 @@
       <button class="shrink-0 w-1/3 min-w-min whitespace-nowrap px-8 py-4 bg-white text-sm text-green-600 font-bold">
         Yêu cầu tư vấn
       </button>
-      <a href="tel:0903324045" class="grow w-2/3 px-6 py-4 border bg-green-600 hover:bg-green-700 text-white text-sm font-bold flex items-center justify-center">
+      <a href="tel:0903324045" class="grow w-2/3 px-6 py-4 h-[58px] bg-green-600 hover:bg-green-700 text-white text-sm font-bold flex items-center justify-center">
         <svg xmlns="http://www.w3.org/2000/svg" class="mr-1 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 3h5m0 0v5m0-5l-6 6M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
         </svg>

4dfb069d36b774bf4ad36dbaeca5854d83396eca
nguyentuankhiem <tuankhiem.maicogroup@gmail.com>
2022-03-22 10:42:23 +0700

    fin fixing: - add 2 folders: filtering and responsive filtering to listing/components - Fix showing project name at edit-project - Fix css file

---
+++ pages/listing/components/responsive-filtering/location-filter-option.vue
@@ -0,0 +1,243 @@
+<template>
+  <div>
+    <div class="w-full flex justify-between border rounded h-11 items-center px-3 mb-4" @click="open = !open">
+      <p class="truncate ...">
+        {{ displaySelected }}
+      </p>
+      <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 13L7 7L1 1" stroke="#999999" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+      </svg>
+    </div>
+    <div
+      v-if="open"
+      ref="dropdownContent"
+      class="fixed inset-0 mt-12 w-full z-30 bg-white opacity-100 overflow-auto"
+    >
+      <div
+        class="flex justify-between border-b rounded"
+      >
+        <div class="absolute top-1.5 left-2" @click="open = false">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="26"
+            height="26"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#32c82b"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ><path d="M19 12H6M12 5l-7 7 7 7" /></svg>
+        </div>
+        <p class="font-semibold px-6 py-2  mr-auto ml-auto">
+          Chọn khu vực
+        </p>
+      </div>
+
+      <div class="relative my-4 w-full px-3">
+        <span class="absolute inset-y-0 left-0 flex items-center pl-4">
+          <button class="p-1 text-gray-400 focus:outline-none focus:shadow-outline">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              viewBox="0 0 24 24"
+              class="w-6 h-6"
+            >
+              <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+            </svg>
+          </button>
+        </span>
+        <input v-model="searchInput" type="search" class="w-full border py-2 pl-10 pr-2 h-full rounded-md bg-white focus:text-gray-900 focus:outline-none" placeholder="Tìm kiếm Tỉnh/Thành phố">
+      </div>
+
+      <div
+        v-if="searchInput.trim() === ''"
+        class="flex justify-between h-11 border-b items-center mx-3"
+        @click="handleSelectAllLocation"
+      >
+        <p class="text-dark-red">
+          Tất cả khu vực
+        </p>
+        <input type="radio" :checked="selectedOption === null">
+      </div>
+      <div
+        v-for="city in filteredCities"
+        :key="city.name"
+        class="flex justify-between h-11 border-b items-center mx-3"
+        @click="handleSelectFirstOption(city)"
+      >
+        <p> {{ city.name }} </p>
+        <svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M1 13L7 7L1 1" stroke="#999999" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+      </div>
+
+      <div
+        v-if="isShowDistrict"
+        class="fixed inset-0 mt-12 w-full z-30 bg-white opacity-100 overflow-auto"
+      >
+        <div
+          class="flex justify-between h-11 border-b items-center mx-3"
+        >
+          <p class="px-4 py-2 bg-green-100 ml-auto mr-auto">
+            {{ firstOption }}
+          </p>
+          <div
+            @click="isShowDistrict = false"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6 relative"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </div>
+        </div>
+
+        <div class="relative my-4 w-full px-3">
+          <span class="absolute inset-y-0 left-0 flex items-center pl-4">
+            <button class="p-1 text-gray-400 focus:outline-none focus:shadow-outline">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                class="w-6 h-6"
+              >
+                <path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+              </svg>
+            </button>
+          </span>
+          <input v-model="searchInput" type="search" class="w-full border py-2 pl-10 pr-2 h-full rounded-md bg-white focus:text-gray-900 focus:outline-none" placeholder="Tìm kiếm Quận/Huyện">
+        </div>
+
+        <div
+          v-for="district in filteredDistricts"
+          :key="district"
+          class="flex justify-between h-11 border-b items-center mx-3"
+          @click="handleSelectSecondOption(district)"
+        >
+          <p>
+            {{ district }}
+          </p>
+          <input type="radio" :checked="isSelected(district)">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { gql } from 'graphql-tag';
+export default {
+  name: 'LocationFilterOption',
+
+  apollo: {
+    projects: gql`
+        query GetProjects {
+          projects {
+            projectName
+            id
+          }
+        }
+    `
+  },
+
+  props: {
+    selectedOption: { type: Object, default: null }
+  },
+
+  data () {
+    return {
+      open: false,
+      entered: false,
+      displaySelected: 'Khu vực',
+      selectedCity: null,
+      firstOption: null,
+      secondOption: null,
+      searchInput: '',
+      isShowDistrict: false,
+      cities: [
+        {
+          name: 'Hồ Chí Minh',
+          districts: ['Quận 9', 'Quận 2', 'Thủ Đức']
+        }
+      ]
+    };
+  },
+
+  computed: {
+    filteredCities () {
+      const query = this.searchInput.trim().toLowerCase();
+      return this.cities.filter(c => c.name.toLowerCase().includes(query));
+    },
+
+    filteredDistricts () {
+      const query = this.searchInput.trim().toLowerCase();
+      return this.selectedCity.districts.filter(d => d.toLowerCase().includes(query));
+    }
+  },
+
+  watch: {
+    selectedOption: {
+      handler (option) {
+        if (option) {
+          this.displaySelected = 'Khu vực: ' + option.city;
+
+          if (option.district) {
+            this.displaySelected += ', ' + option.district;
+          }
+        } else {
+          this.displaySelected = 'Khu vực';
+        }
+      },
+      immediate: true
+    }
+  },
+
+  methods: {
+    isSelected (district) {
+      return district === this.selectedOption?.district;
+    },
+
+    handleSelectFirstOption (city) {
+      this.isShowDistrict = true;
+      this.selectedCity = city;
+      this.firstOption = city.name;
+      this.searchInput = '';
+    },
+
+    handleSelectAllLocation () {
+      this.$emit('optionchanged', null);
+      this.open = false;
+    },
+
+    handleSelectSecondOption (option) {
+      this.secondOption = option;
+      this.$emit('optionchanged', { city: this.firstOption, district: this.secondOption });
+      this.isShowDistrict = false;
+      this.open = false;
+    },
+
+    handleGoBack () {
+      this.selectedCity = null;
+      this.firstOption = null;
+      this.$emit('optionchanged', null);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.text-dark-red {
+  color: #961B12;
+}
+</style>

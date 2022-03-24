10951b0863a4e8dea60462727d82a40c3d3161fc
Hồ Quang Lâm <100085404+quanglam2003@users.noreply.github.com>
2022-03-25 05:13:11 +0700

    feature/add-page-loading-animation-and-home-logo-href-and-animation (#43)
    * add page loading animation and home page logo href
    
    * update http endpoint same as master
    
    * update to nuxt link
    
    * update mobile sidebar animation
    
    * update loading 4000ms at default.vue
    
    * update default.vue as master and then add 4000ms loading
    
    * update ban-&-cho-thue toogle button animation
    
    * shorten animation
    
    * update loading at detail page and update location and time at post listings mobile
    
    * fix conflict master
    
    * fix conflict master 2
    
    * add favicon logo, add project overview, fix post address title at mobile
    
    * fix overview project height at PC
    
    * fix project header
    
    * fix loading at posts list (pc) and trying to fix loading at detail post
    
    * fix mobile skeleton loading mobile
    
    * fix loading detail post page
    
    * update margin

--- layouts/components/mainHeader/MainHeader.vue
+++ layouts/components/mainHeader/MainHeader.vue
@@ -1,12 +1,24 @@
 <template>
   <div
-    class="fixed z-20 top-0 left-0 right-0 bg-white flex justify-between items-center px-5 py-3 border-b"
+    class="
+      fixed
+      z-20
+      top-0
+      left-0
+      right-0
+      bg-white
+      flex
+      justify-between
+      items-center
+      px-5
+      py-3
+      border-b
+    "
   >
     <NuxtLink to="/">
       <img onclick="void(0)" id="logo" class="h-12" src="~/assets/maico.png">
     </NuxtLink>
-    
-
+   
     <div class="hidden md:flex flex justify-between items-center space-x-10">
       <a class="font-bold hidden xl:block" href="tel:0903324045">
         0903 324 045
@@ -55,12 +67,20 @@
       </Dropdown>
 
       <Dropdown class="hidden lg:inline" item-width="220px" title="Dự án">
-        <div class=" max-h-96 overflow-y-auto overflow-x-hidden">
+        <div class="max-h-96 overflow-y-auto overflow-x-hidden">
           <DropdownItem> Tất cả dự án </DropdownItem>
           <nuxt-link
             v-for="project in projects"
             :key="project.id"
-            class="block px-4 py-2 capitalize text-gray-700 hover:bg-blue-500 hover:text-white cursor-pointer"
+            class="
+              block
+              px-4
+              py-2
+              capitalize
+              text-gray-700
+              hover:bg-blue-500 hover:text-white
+              cursor-pointer
+            "
             :to="`/danh-sach-can-ho/${project.pageInfors[0].slug}`"
           >
             {{ project.projectName }}
@@ -84,7 +104,16 @@
 
         <a href="#" class="ml-6"> Đăng ký </a>
 
-        <button class="ml-10 border rounded-md py-2 px-8 hover:text-white hover:bg-gray-700">
+        <button
+          class="
+            ml-10
+            border
+            rounded-md
+            py-2
+            px-8
+            hover:text-white hover:bg-gray-700
+          "
+        >
           Ký gửi
         </button>
       </div>
@@ -107,72 +136,107 @@
     </button>
 
     <div
-      v-if="showSidebar"
-      class="fixed inset-0 h-full w-full z-10 bg-black opacity-20"
+      v-show="showSidebar != null"
+      :class="`fixed inset-0 h-full w-full z-10 bg-black opacity-20 ${(showSidebar)? 'visible' : 'hide-side-bar-parent'}`"
       @click="showSidebar = false"
     />
 
-    <Sidebar v-if="showSidebar" :projects="projects" class="z-20" />
+    <Sidebar v-show="showSidebar != null" :projects="projects" :class="`z-20 ${(showSidebar)? 'show-side-bar' : 'hide-side-bar'}`" />
   </div>
 </template>
 
 <script>
-import { gql } from 'graphql-tag';
-import Sidebar from './Sidebar.vue';
-import DropdownItem from '~/components/dropdown/DropdownItem.vue';
+import { gql } from "graphql-tag";
+import Sidebar from "./Sidebar.vue";
+import DropdownItem from "~/components/dropdown/DropdownItem.vue";
 
 export default {
-  name: 'MainHeader',
+  name: "MainHeader",
   components: { Sidebar, DropdownItem },
 
   apollo: {
     projects: gql`
-        query GetProjects {
-          projects {
-            projectName
-            pageInfors {
-              slug
-            }
+      query GetProjects {
+        projects {
+          projectName
+          pageInfors {
+            slug
           }
         }
-    `
+      }
+    `,
   },
-  mounted(){
-    if(process.client){
-      const logoEle = document.getElementById('logo');
+  mounted() {
+    if (process.client) {
+      const logoEle = document.getElementById("logo");
 
-      logoEle.addEventListener('click', this.disableCookies);
+      logoEle.addEventListener("click", this.disableCookies);
     }
   },
-  data () {
-    return { showSidebar: false };
+  data() {
+    return { showSidebar: null };
   },
 
   methods: {
-    handleSelecType (type, demand) {
-      const path = '/danh-sach-can-ho';
+    handleSelecType(type, demand) {
+      const path = "/danh-sach-can-ho";
       const query = {
         loai: type,
-        demand
+        demand,
       };
 
       this.$router.push({ path, query });
     },
-    disableCookies(evt){
+    disableCookies(evt) {
       if (evt.detail === 3) {
-            this.$cookies.set('trackingState','disabled',{
-              path: '/',
-              maxAge: 60 * 60 * 24 * 365
-            });
-            alert('Đã tắt tracking');
-          }
+        this.$cookies.set("trackingState", "disabled", {
+          path: "/",
+          maxAge: 60 * 60 * 24 * 365,
+        });
+        alert("Đã tắt tracking");
+      }
     },
-    handleSelectAllTypes (demand) {
-      const path = '/danh-sach-can-ho';
+    handleSelectAllTypes(demand) {
+      const path = "/danh-sach-can-ho";
       const query = { demand };
 
       this.$router.push({ path, query });
-    }
-  }
+    },
+  },
 };
 </script>
+
+<style scoped>
+.show-side-bar{
+  animation: slide-in 0.25s ease;
+}
+
+.hide-side-bar-parent{
+  visibility: hidden;
+}
+
+.hide-side-bar{
+  animation: slide-out 0.25s ease;
+  width: 0;
+}
+
+@keyframes slide-in {
+  from{
+    width: 0;
+  }
+  to{
+    /*width: 0.25 * 64 ~~ w-64 in tailwind */
+    width: calc(0.25rem * 64);
+  }
+}
+
+@keyframes slide-out {
+  from{
+    /*width: 0.25 * 64 ~~ w-64 in tailwind */
+    width: calc(0.25rem * 64);  
+  }
+  to{
+    width: 0;
+  }
+}
+</style>
